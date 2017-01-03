16f699c854d04eae215bf8945684b9f8ae7a4ed4
Lyndon McKay <lyndonmckay13@gmail.com>
2017-01-03 13:15:06 -0600

    Update Profile Picture options

--- app/api/api/v1/users.rb
+++ app/api/api/v1/users.rb
@@ -115,7 +115,7 @@ module API
               # set the image
               @picture = existing_user.image
 
-                image_file = Paperclip.io_adapters.for(params[:photo_path][:file_data])
+                image_file = Paperclip.io_adapters.for(params[:photo_path][:data])
                 image_file.original_filename = existing_user.name #params[:file_name]
                 # image_file.content_type = "image/jpeg"
                 @picture = image_file
