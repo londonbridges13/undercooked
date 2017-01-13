4f6ea2ee083a5901c0341d86fc6f72c477f40890
Lyndon McKay <lyndonmckay13@gmail.com>
2017-01-13 15:14:18 -0600

    API Tweaks

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -87,7 +87,7 @@ module TopicsHelper
       # the goal is to get three articles per topic
 
       featured_topic = Topic.where(:id => 4).first # the id of te featured_topic should be four 1/13/17
-      featured_articles = featured_topic.articles.where(:publish_it => true).sort_by(&:created_at).reverse.limit(5).all
+      featured_articles = featured_topic.articles.where(:publish_it => true).limit(5).sort_by(&:created_at).reverse
       done = false
       i = 0
       while i < featured_articles.count and done == false
