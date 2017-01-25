047977fd9f6413e79bfe6c2fc05ccd14facb8034
Lyndon McKay <lyndonmckay13@gmail.com>
2017-01-24 19:29:41 -0600

    API Tweaks

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -12,7 +12,7 @@ module ArticlesHelper
 
   def check_resource(resource) #resource object here
     # Check for articles in this resource
-    feed = Feedjira::Feed.fetch_and_parse resource.resource_url#force_encoding('UTF-8')
+    feed = Feedjira::Feed.fetch_and_parse "http://thevietvegan.com//feed"#resource.resource_url#force_encoding('UTF-8')
     feed.entries.each do |entry|
       i = 0
       while i < 3
