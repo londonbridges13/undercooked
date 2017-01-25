2a7db985cf1df1574c3cfc6ea5d47a4f1ed1b962
Lyndon McKay <lyndonmckay13@gmail.com>
2017-01-24 20:36:10 -0600

    API Tweaks

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -13,7 +13,7 @@ module ArticlesHelper
   def check_resource(resource) #resource object here
     # Check for articles in this resource
     url =  "http://feeds.feedburner.com/MinimalistBaker?format=xml"
-    xml = Faraday.get(url).body
+    xml = Faraday.get(url)
     feed = Feedjira::Feed.fetch_and_parse xml#resource.resource_url#force_encoding('UTF-8')
     feed.entries.each do |entry|
       i = 0
