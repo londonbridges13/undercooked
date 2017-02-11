47bfe98f7b6503e34e04aec26fa996aa2ae96946
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-11 12:40:04 -0600

    API Tweaks

--- app/api/api/v2/resources.rb
+++ app/api/api/v2/resources.rb
@@ -8,6 +8,7 @@ module API
         doorkeeper_authorize!
       end
       format :json
+      helpers ResourcesHelper
 
       resource :resources do
         namespace 'display_resource' do
@@ -17,7 +18,106 @@ module API
             article = Article.find_by_id(id)
             resource = article.resource
             present resource
-            
+
+          end
+        end
+      end
+
+      resource :resources do
+        namespace 'display_resources' do
+          desc "Query all Resource"
+          post do
+            resources = Resource.all
+            present resources
+          end
+        end
+      end
+
+      resource :resources do
+        namespace 'get_resource_info' do
+          desc "Query a Resource"
+          post do
+            resource_id = params[:resource_id]
+            resource = Resource.find_by_id(resource_id)
+            present resource
+          end
+        end
+      end
+
+      resource :resources do
+        namespace 'display_resource_articles' do
+          desc "Query Resource's Articles"
+          post do
+            resource_id = params[:resource_id]
+            resource = Resource.find_by_id(resource_id)
+            present resource.articles
+          end
+        end
+      end
+
+
+      resource :resources do
+        namespace 'test_resource' do
+          desc "Query a Resource"
+          post do
+            resource_id = params[:resource_id]
+            resource = Resource.find_by_id(resource_id)
+
+            check_resource(resource)
+
+          end
+        end
+      end
+
+
+      resource :resources do
+        namespace 'get_resource_topics' do
+          desc "Query Resource's Topics"
+          post do
+            id = params[:resource_id]
+            resource = Resource.find_by_id(id)
+            present resource.topics
+          end
+        end
+      end
+
+      resource :resources do
+        namespace 'update_title_desc' do
+          desc "Update Desc and Title of an Article"
+          post do
+            id = params[:resource_id]
+            title = params[:title]
+            desc = params[:desc]
+
+            resource = Resource.find_by_id(id)
+            resource.desc = desc
+            resource.title = title
+            resource.save
+            present resource
+          end
+        end
+      end
+
+
+      resource :resources do
+        namespace 'link_to_topics' do
+          desc "Connect Resource to Topics"
+          post do
+            resource_id = params[:resource_id]
+            resource = Resource.find_by_id(resource_id)
+
+            topics = params[:topics]
+
+            resource.topics.delete_all
+
+            topics.each do |t|
+              topic = Topic.find_or_create_by(title: t)
+              unless resource.topics.include? topic
+                resource.topics.push(topic)
+              end
+            end
+            present topics
+
           end
         end
       end
--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,77 @@
 module ResourcesHelper
+
+  def test_resource(resource)
+    # search the url for articles, but don't create anything
+    check_resource(resource)
+  end
+
+  def check_resource(resource) #should be the same as ArticlesHelper
+    if resource.resource_url.include? "youtube.com"
+      # Check for videos in this resource
+      get_youtube_videos(resource)
+    elsif resource.resource_url.include? "autoimmunewellness.com"
+      # the weird articles that cause errors
+      get_other_articles(resource)
+    else
+      # Check for articles in this resource
+      get_articles(resource)
+    end
+  end
+
+
+
+  #Check Resource Functions
+
+    def get_youtube_videos(resource)
+      # this gets the other a youtube channel's videos using Feedjira::fetch_and_parse
+      # resource.resource_url.include? "youtube.com" , should be true
+        # Check for videos in this resource
+        #able_to_parse
+        url =  resource.resource_url#"http://feeds.feedburner.com/MinimalistBaker?format=xml"
+        xml = Faraday.get(url).body.force_encoding('utf-8')
+        feed = Feedjira::Feed.fetch_and_parse xml#resource.resource_url#force_encoding('UTF-8')
+        if feed.entries.count > 0
+          present "Successful Test"
+
+        else
+          present "Found Nothing, but still Successful"
+        end
+    end
+
+    def get_articles(resource)
+      # this gets the other articles using Feedjira::parse
+      # Check for articles in this resource
+      url =  resource.resource_url#"http://feeds.feedburner.com/MinimalistBaker?format=xml"
+      xml = Faraday.get(url).body.force_encoding('utf-8')
+      puts url
+      feed = Feedjira::Feed.parse xml#url#resource.resource_url#force_encoding('UTF-8')
+      if feed.entries.count > 0
+        present "Successful Test"
+
+      else
+        present "Found Nothing, but still Successful"
+      end
+    end
+
+
+
+    def get_other_articles(resource)
+      # this gets the other articles using Feedjira::fetch_and_parse
+
+      url =  resource.resource_url#"http://feeds.feedburner.com/MinimalistBaker?format=xml"
+      # xml = Faraday.get(url).body.force_encoding('utf-8')
+      puts url
+      feed = Feedjira::Feed.fetch_and_parse url#resource.resource_url#force_encoding('UTF-8')
+      if feed.entries.count > 0
+        present "Successful Test"
+
+      else
+        present "Found Nothing, but still Successful"
+      end
+    end
+
+
+
+
+
 end
