e8b39393962b6f07bac46880fac8523facad02cd
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-13 01:53:01 -0600

    API Tweaks

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -3,7 +3,7 @@ module SuggestionsHelper
 
   def create_suggestions_for_topic(topic)
     two_days_ago = Time.now - 3.days
-    all_recent_articles = Article.where('article_date < ?', two_days_ago).potential_suggested_articles #test, not working
+    all_recent_articles = Article.where('article_date < ?', 3.days.ago)#.potential_suggested_articles #test, not working
 
     existing_suggestions = []
     topic.suggestions.each do |a|
