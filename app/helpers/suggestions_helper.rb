04e894f99b738ed1f6c0447a04bc82e97ffc9e5a
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-13 01:38:10 -0600

    API Tweaks

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -3,30 +3,43 @@ module SuggestionsHelper
 
   def create_suggestions_for_topic(topic)
     two_days_ago = Time.now - 3.days
-    all_recent_articles = Article.potential_suggested_articles.where('article_date < ?', two_days_ago) #test
+    all_recent_articles = Article.potential_suggested_articles.where('article_date < ?', two_days_ago) #test, not working
+
+    existing_suggestions = []
+    topic.suggestions.each do |a|
+      unless existing_suggestions.include? a
+        existing_suggestions.push a
+      end
+    end
 
     all_recent_articles.each do |a|
         #Resource
       if topic.resources.include? a.resource
         #create suggestions
-        new_suggestion = topic.suggestions.build(:reason => "Resource", :evidence => a.resource.title)
-        new_suggestion.article = a
-        new_suggestion.save
+        unless existing_suggestions.include? a
+          new_suggestion = topic.suggestions.build(:reason => "Resource", :evidence => a.resource.title)
+          new_suggestion.article = a
+          new_suggestion.save
+        end
       else
         #Keyword
         topic.keywords.each do |k|
           # see if the keyword exists in in the article's desc or title
           if a.title.include? k
             #create suggestion
-            new_suggestion = topic.suggestions.build(:reason => "Keyword", :evidence => k)
-            new_suggestion.article = a
-            new_suggestion.save
+            unless existing_suggestions.include? a
+              new_suggestion = topic.suggestions.build(:reason => "Keyword", :evidence => k)
+              new_suggestion.article = a
+              new_suggestion.save
+            end
 
           elsif a.desc.include? k
             #create suggestion
-            new_suggestion = topic.suggestions.build(:reason => "Keyword", :evidence => k)
-            new_suggestion.article = a
-            new_suggestion.save
+            unless existing_suggestions.include? a
+              new_suggestion = topic.suggestions.build(:reason => "Keyword", :evidence => k)
+              new_suggestion.article = a
+              new_suggestion.save
+            end
           end
         end
 
