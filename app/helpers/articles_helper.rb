c89b73333876acf9f831628ca51bf89981231c95
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-16 09:56:54 -0600

    API Tweaks

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -76,7 +76,7 @@ module ArticlesHelper
 
   def add_articles(topic)
     two_days_ago = Time.now - 3.days # change back to 3
-    potential_articles = topic.articles.where('article_date > ? AND publish_it = 1', two_days_ago)
+    potential_articles = topic.articles.where('article_date > ?', two_days_ago).where(:publish_it => true)
     if potential_articles.count > 2 # 3 is enough
       # add each to @articles if they aren't in the article
       potential_articles.each do |a|
