5319ce4c7388aa7f4e987c84619712768d260808
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-20 08:17:50 -0600

    API PI Tweaks

---
+++ app/api/api/v1/feedbacks.rb
@@ -0,0 +1,34 @@
+require 'doorkeeper/grape/helpers'
+
+module API
+  module V2
+    class Feedbacks < Grape::API
+      helpers Doorkeeper::Grape::Helpers
+      before do
+        doorkeeper_authorize!
+      end
+      format :json
+
+      resource :feedbacks do
+        namespace 'feedback' do
+          desc "Sends feedback and suggestion from user"
+          post do
+              # find Article
+              suggestion = params[:suggestion]
+              feedback = params[:feedback]
+
+              if feedback
+                #create feedback here (message, suggestion)
+                new_feedback.new(:message => feedback, :suggestion => suggestion)
+                new_feedback.save
+                present "Saved"
+              else
+                present "Error"
+              end
+          end
+        end
+      end
+
+    end
+  end
+end
--- app/api/api/v1/users.rb
+++ app/api/api/v1/users.rb
@@ -223,25 +223,7 @@ module API
       end
 
 
-      resource :users do
-        namespace 'feedback' do
-          desc "Sends feedback and suggestion from user"
-          post do
-              # find Article
-              suggestion = params[:suggestion]
-              feedback = params[:feedback]
-
-              if feedback 
-                #create feedback here (message, suggestion)
-                new_feedback.new(:message => feedback, :suggestion => suggestion)
-                new_feedback.save
-                present "Saved"
-              else
-                present "Error"
-              end
-          end
-        end
-      end
+      
 
 
 
