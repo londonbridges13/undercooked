d517c2e58aaffdb3bd7ebfe586b2c64008cbb67e
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-28 12:02:02 -0600

    API Tweaks

--- app/api/api/v1/topics.rb
+++ app/api/api/v1/topics.rb
@@ -155,7 +155,7 @@ module API
         namespace 'get_topics' do
           desc "Query User's Topics"
           post do
-            token = params[:utoken]
+            token = params[:utoken] 
             #find user by token
             current_user = Topic.find_by_access_token(token)
             #make sure current_user exists
