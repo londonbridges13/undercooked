c7e7306c554f1426caf94e49c8c142e7592a3193
Lyndon McKay <lyndonmckay13@gmail.com>
2017-02-28 20:54:02 -0600

    API Tweaks

--- app/jobs/content_worker.rb
+++ app/jobs/content_worker.rb
@@ -5,7 +5,7 @@ class ContentWorkerController
   def perform(topic_id)
     #ActiveRecord::Base.connection_pool.with_connection do
       topic = Topic.find_by_id(topic_id)
-      present topic_id
+      present "topic_id"
   #  end
   end
 
