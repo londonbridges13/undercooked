3b200794cc1c4cd0c77b2a352b730c560111dc1d
Lyndon McKay <lyndonmckay13@gmail.com>
2017-03-05 01:28:11 -0600

    API Tweaks

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -3,7 +3,7 @@ module SuggestionsHelper
 
   def create_suggestions_for_topic(topic)
     remove_accepted_suggestions(topic)
-    all_recent_articles = Article.where('article_date > ?', 3.days.ago).where("publish_it != ? OR publish_it IS NULL",false)run
+    all_recent_articles = Article.where('article_date > ?', 3.days.ago).where("publish_it != ? OR publish_it IS NULL",false) #test, not working with scope
 
     existing_suggested_articles = [] # get existing suggestions
     existing_topic_articles = topic.articles.where('article_date > ?', 3.days.ago) # get recent articles from topic
