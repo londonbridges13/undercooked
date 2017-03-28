3ddbcad40a42a0b111092b5c5da9777b11487efb
Lyndon McKay <lyndonmckay13@gmail.com>
2017-03-28 18:13:05 -0500

    API Tweaks

--- app/jobs/content_worker.rb
+++ app/jobs/content_worker.rb
@@ -48,7 +48,7 @@ class ContentWorker
         if resource.resource_type == "error"
           #do nothing
           puts "This resoruce has it's own error"
-        elsif resource.resource_url.include? "autoimmunewellness.com" or resource.resource_type == "article-xml"
+        elsif resource.resource_type == "article-xml" #or resource.resource_url.include? "autoimmunewellness.com"
           # the weird articles that cause errors
           get_other_articles(resource)
         elsif resource.resource_url.include? "youtube.com" or resource.resource_type == "video"
@@ -193,6 +193,15 @@ class ContentWorker
           i += 1
         end
       end
+    end
+
+
+
+    # Check for Recipe inside the new article (Conversion). If possible then convert the recipe
+
+    def convert_article
+
+
     end
 
 
