707ce913a84f0e3a48dcb28314180a76dacac3ae
Lyndon McKay <lyndonmckay13@gmail.com>
2017-05-25 18:27:46 -0500

    API Tweaks

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -89,7 +89,9 @@ module SuggestionsHelper
       # get article suggestions
       if topic.articles.include? s.article
         # remove the suggestion
-        s.auto_publishing.delete 
+        s.auto_publishing.delete
+        s.auto_publishing = nil
+        s.save 
         s.delete # WE DON'T NEED TO KEEP A SUGGESTION IF IT WAS ACCEPTED
       end
     end
