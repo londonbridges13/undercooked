0f9d6bba961221514c40ad44c39cfc33dd658825
Lyndon McKay <lyndonmckay13@gmail.com>
2017-05-31 14:52:41 -0500

    Set Handpicked Query

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -174,9 +174,9 @@ module ArticlesHelper
       end
 
       topics = user.topics
-      x_days_ago = 23
+      x_days = 23
       topics.each do |t|
-        t.articles.where('article_date > ?', x_days_ago).order('article_date DESC').limit(3).each do |a|
+        t.articles.where('article_date > ?', x_days.ago).order('article_date DESC').limit(3).each do |a|
           unless all_articles.include? a
             all_articles.push a
           end
